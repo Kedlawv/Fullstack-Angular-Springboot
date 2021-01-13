ff34251428270ebaeac6065d1f7ade95d38d1d16
Kedlawv <waldek.bus@gmail.com>
2021-01-13 19:30:23 +0100

    Notes & Welcome test component & login component  -> data binding examples & anguar directive -> [(ngModel)]

---
+++ frontend/todo/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = 'kadlaw';
+  password = '';
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  handleLogin(): void{
+    console.log(this.username);
+    console.log(this.password);
+  }
+
+}
