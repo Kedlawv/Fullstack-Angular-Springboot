de6c5e25f280603f648d639f7fdf7d4661edf0af
Kedlawv <waldek.bus@gmail.com>
2021-01-17 15:13:21 +0100

    Path Variable example

--- restful-web-services/src/main/java/com/kedlaw/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/kedlaw/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.kedlaw.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,15 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World from Bean!");
+    }
+
+    @GetMapping("/hello-name-param/{name}")
+    public String helloNameParameter(@PathVariable String name){
+        return "Hello " + name;
+    }
+
+    @GetMapping("/hello-bean-name/{name}")
+    public HelloWorldBean helloBeanName(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello %s",name));
     }
 }
