4686375945199ae907e40659dccefbffb96c0cb5
Kedlawv <waldek.bus@gmail.com>
2021-01-22 22:58:29 +0100

    Rest controller Post todo & Put todo

--- restful-web-services/src/main/java/com/kedlaw/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ restful-web-services/src/main/java/com/kedlaw/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -27,7 +27,7 @@ public class TodoHardcodedService {
         if (todo == null) {
             return null;
         }
-        if(todos.remove(todo)){
+        if (todos.remove(todo)) {
             return todo;
         }
         return null;
@@ -41,6 +41,17 @@ public class TodoHardcodedService {
         }
 
         return null;
+    }
+
+    public Todo save(Todo todo) {
+        if (todo.getId() == -1 || todo.getId() == 0) {
+            todo.setId(++idCounter);
+            todos.add(todo);
+        } else {
+            deleteById(todo.getId());
+            todos.add(todo);
+        }
+        return todo;
     }
 
 
--- restful-web-services/src/main/java/com/kedlaw/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/kedlaw/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,30 +1,33 @@
 package com.kedlaw.rest.webservices.restfulwebservices.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TodoResource {
 
     @Autowired
-    private TodoHardcodedService todoHardcodedService ;
+    private TodoHardcodedService todoHardcodedService;
 
     @GetMapping("/users/{username}/todos")
-    public List<Todo> getAllTodos(@PathVariable String username){
+    public List<Todo> getAllTodos(@PathVariable String username) {
         return todoHardcodedService.findAll();
     }
 
     @GetMapping("/users/{username}/todos/{id}")
-    public Todo getTodoById(@PathVariable String username, @PathVariable long id){
+    public Todo getTodoById(@PathVariable String username, @PathVariable long id) {
         return todoHardcodedService.findById(id);
     }
 
     @DeleteMapping("/users/{username}/todos/{id}")
-    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
         Todo todo = todoHardcodedService.deleteById(id);
         if (todo != null) {
             return ResponseEntity.noContent().build();
@@ -32,4 +35,23 @@ public class TodoResource {
 
         return ResponseEntity.notFound().build();
     }
+
+    @PutMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id,
+                                           @RequestBody Todo todo) {
+
+        Todo todoUpdated = todoHardcodedService.save(todo);
+        return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public ResponseEntity<Void> saveTodo(@PathVariable String username, @RequestBody Todo todo) {
+
+        Todo todoSaved = todoHardcodedService.save(todo);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(todoSaved.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
 }
